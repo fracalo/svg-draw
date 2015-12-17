33b85b6fd4256a53c1648c2a0cdc7bd97ef1e488
fracalo <premispezia@gmail.com>
2015-12-17 15:54:41 +0100

    travis

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome', 'Firefox'],
+    browsers : ['Firefox'],
 
     plugins : [
             'karma-chrome-launcher',
